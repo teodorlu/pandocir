8ed0660269c57f78bcde5cf8231bebc608615897
Teodor Heggelund <git@teod.eu>
2024-05-12 09:16:21 +0200

    More babashka.cli examples

--- test/teodorlu/pandoc2hiccup_test.clj
+++ test/teodorlu/pandoc2hiccup_test.clj
@@ -12,12 +12,22 @@
     (is (= {:from "markdown", :to "json"}
            (babashka.cli/parse-opts ["--from" "markdown" "--to" "json"]))))
   (testing "for scripts with subcommands, consider using babashka.cli/dispatch."
-    (let [fruit-dispatch-table
-          [{:cmds ["apples"]
-            :fn (fn [_opts] "there are apples")}
-           {:cmds ["oranges"]
-            :fn (fn [_opts] "oranges give vitamin C!")}]]
-      (is (= "there are apples"
-             (babashka.cli/dispatch fruit-dispatch-table ["apples"])))))
-
-  )
+    (let [apples-return-value "there are apples"
+          oranges-return-value "oranges give vitamin C!"
+          fruit-dispatch-table [{:cmds ["apples"]
+                                 :fn (fn [_opts+args] apples-return-value)}
+                                {:cmds ["oranges"]
+                                 :fn (fn [_opts+args] oranges-return-value)}]]
+      (is (= apples-return-value
+             (babashka.cli/dispatch fruit-dispatch-table ["apples"])))
+      (is (= oranges-return-value
+             (babashka.cli/dispatch fruit-dispatch-table ["oranges"])))))
+  (testing "subcommands can also receive options. Option value types are inferred."
+    (is (= 42
+           (babashka.cli/dispatch [{:cmds ["mycmd"]
+                                    :fn (fn [opts+args]
+                                          (:count (:opts opts+args)))}]
+                                  ["mycmd" "--count" "42"]))))
+  (testing "opts+args is just a map."
+    (is (map? (babashka.cli/dispatch [{:cmds ["mycmd"] :fn identity}]
+                                     ["mycmd" "--count" "42"])))))
