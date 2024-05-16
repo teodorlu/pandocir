13475492accd6de0b682ed677bbf9969e04d47ec
Teodor Heggelund <git@teod.eu>
2024-05-16 14:18:44 +0200

    Mob-programming at Oslo Clojure meetup: Babashka i praksis

--- src/teodorlu/pandoc2hiccup.clj
+++ src/teodorlu/pandoc2hiccup.clj
@@ -1,15 +1,52 @@
-(ns teodorlu.pandoc2hiccup)
+(ns teodorlu.pandoc2hiccup
+  (:require [cheshire.core :as json]))
 
 (defn a-function [] 42)
 
+(json/parse-string
+ "{\"pandoc-api-version\":[1,23,1],\"meta\":{},\"blocks\":[{\"t\":\"Para\",\"c\":[{\"t\":\"Str\",\"c\":\"hi,\"},{\"t\":\"Space\"},{\"t\":\"Emph\",\"c\":[{\"t\":\"Str\",\"c\":\"there\"}]},{\"t\":\"Str\",\"c\":\"!\"}]}]}\n" keyword)
+
+(json/parse-string
+ "{\"pandoc-api-version\":[1,23,1],\"meta\":{},\"blocks\":[{\"t\":\"Para\",\"c\":[{\"t\":\"Str\",\"c\":\"hei\"}]},{\"t\":\"Para\",\"c\":[{\"t\":\"Str\",\"c\":\"oslo\"},{\"t\":\"Space\"},{\"t\":\"Str\",\"c\":\"clojure\"}]}]}\n"
+ keyword)
+
+{:pandoc-api-version [1 23 1],
+ :meta {},
+ :blocks
+ [{:t "Para", :c [{:t "Str", :c "hei"}]}
+  {:t "Para",
+   :c [{:t "Str", :c "oslo"} {:t "Space"} {:t "Str", :c "clojure"}]}]}
+
+[:p "hi," " " [:em "there"] "!"]
+{:pandoc-api-version [1 23 1],
+ :meta {},
+ :blocks
+ [{:t "Para",
+   :c
+   [{:t "Str", :c "hi,"}
+    {:t "Space"}
+    {:t "Emph", :c [{:t "Str", :c "there"}]}
+    {:t "Str", :c "!"}]}]}
+
+(defn pandoc-block->hiccup [{:keys [t c]}]
+  (case t
+    "Space" " "
+    "Str" c
+    "Emph" (into [:em] (map pandoc-block->hiccup c))
+    "Para" (into [:p] (map pandoc-block->hiccup c))))
+
+(defn pandoc->hiccup [{:keys [blocks]}]
+  (map pandoc-block->hiccup blocks))
+
 (defn -main [& args]
-  (println "hello from pandoc2hiccup")
-  (println "got these args:")
-  (println (str "\n\n    " (pr-str args) "\n"))
-  (println "Now waiting for stdin.")
+  ;; (println "hello from pandoc2hiccup")
+  ;; (println "got these args:")
+  ;; (println (str "\n\n    " (pr-str args) "\n"))
+  ;; (println "Now waiting for stdin.")
   (let [in (slurp System/in)]
-    (println "stdin:")
-    (prn in))
-  (println)
-  (println "Done.")
+    (prn (pandoc->hiccup (json/parse-string in keyword)))
+    ;; (println "stdin:")
+    #_(prn in))
+  ;; (println)
+  ;; (println "Done.")
   (println "This text was printed for demonstration purposes."))
--- test/teodorlu/pandoc2hiccup_test.clj
+++ test/teodorlu/pandoc2hiccup_test.clj
@@ -6,3 +6,35 @@
 
 (deftest a-function-test
   (is (= 42 (pandoc2hiccup/a-function))))
+
+(deftest space-test
+  (is (= " " (pandoc2hiccup/pandoc-block->hiccup {:t "Space"}))))
+
+(deftest string-test
+  (is (= "there"
+         (pandoc2hiccup/pandoc-block->hiccup {:t "Str", :c "there"}))))
+
+(deftest emph-test
+  (is (= [:em "there"]
+         (pandoc2hiccup/pandoc-block->hiccup
+          {:t "Emph", :c [{:t "Str", :c "there"}]}))))
+
+(deftest para-test
+  (is (= [:p "hi," " " [:em "there"] "!"]
+         (pandoc2hiccup/pandoc-block->hiccup
+          {:t "Para",
+           :c
+           [{:t "Str", :c "hi,"}
+            {:t "Space"}
+            {:t "Emph", :c [{:t "Str", :c "there"}]}
+            {:t "Str", :c "!"}]}))))
+
+(deftest hiccup-test
+  (is (= '([:p "hei"] [:p "oslo" " " "clojure"])
+         (pandoc2hiccup/pandoc->hiccup
+          {:pandoc-api-version [1 23 1],
+           :meta {},
+           :blocks
+           [{:t "Para", :c [{:t "Str", :c "hei"}]}
+            {:t "Para",
+             :c [{:t "Str", :c "oslo"} {:t "Space"} {:t "Str", :c "clojure"}]}]}))))
