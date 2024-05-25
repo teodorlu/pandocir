fca6bf1d308b146ff3bb5483b2c85d2fa48783df
larstvei <larstvei@ifi.uio.no>
2024-05-25 16:37:35 +0200

    A simpler and more general way of generating an IR
    Each pandoc type specifies a namespaced keyword identifying the type, and a
    list of namespaced keywords for the child elements for that type.

--- src/pandocir/ir.clj
+++ src/pandocir/ir.clj
@@ -1,85 +1,35 @@
 (ns pandocir.ir
   (:require [clojure.walk :as walk]))
 
-(def ^:private type->ir
-  {"Str"         :pandocir.type/str
-   "Emph"        :pandocir.type/emph
-   "Underline"   :pandocir.type/underline
-   "Strong"      :pandocir.type/strong
-   "Strikeout"   :pandocir.type/strikeout
-   "Superscript" :pandocir.type/superscript
-   "Subscript"   :pandocir.type/subscript
-   "SmallCaps"   :pandocir.type/small-caps
-   "Quoted"      :pandocir.type/quoted
-   "Cite"        :pandocir.type/cite
-   "Code"        :pandocir.type/code
-   "Space"       :pandocir.type/space
-   "SoftBreak"   :pandocir.type/soft-break
-   "LineBreak"   :pandocir.type/line-break
-   "Math"        :pandocir.type/math
-   "RawInline"   :pandocir.type/raw-inline
-   "Link"        :pandocir.type/link
-   "Image"       :pandocir.type/image
-   "Note"        :pandocir.type/note
-   "Span"        :pandocir.type/span})
+(def ^:private pandoc-types
+  {"Str"            [:pandocir.type/str :pandocir/text]
+   "Emph"           [:pandocir.type/emph :pandocir/inlines]
+   "Underline"      [:pandocir.type/underline :pandocir/inlines]
+   "Strong"         [:pandocir.type/strong :pandocir/inlines]
+   "Strikeout"      [:pandocir.type/strikeout :pandocir/inlines]
+   "Superscript"    [:pandocir.type/superscript :pandocir/inlines]
+   "Subscript"      [:pandocir.type/subscript :pandocir/inlines]
+   "SmallCaps"      [:pandocir.type/small-caps :pandocir/inlines]
+   "Quoted"         [:pandocir.type/quoted :pandocir.quote/type :pandocir/inlines]
+   "Cite"           [:pandocir.type/cite :pandocir/citations :pandocir/inlines]
+   "Code"           [:pandocir.type/code :pandocir/attr :pandocir/text]
+   "Space"          [:pandocir.type/space]
+   "SoftBreak"      [:pandocir.type/soft-break]
+   "LineBreak"      [:pandocir.type/line-break]
+   "Math"           [:pandocir.type/math :pandocir.math/type :pandocir/text]
+   "RawInline"      [:pandocir.type/raw-inline :pandocir/format :pandocir/text]
+   "Link"           [:pandocir.type/link :pandocir/attr :pandocir/inlines :pandocir/target]
+   "Image"          [:pandocir.type/image :pandocir/attr :pandocir/inlines :pandocir/target]
+   "Note"           [:pandocir.type/note :pandocir/blocks]
+   "Span"           [:pandocir.type/span :pandocir/attr :pandocir/inlines]})
 
+(defn ^:private pandoc->ir-1 [{:keys [t c] :as x}]
+  (if-let [[pandocir-type & args] (get pandoc-types t)]
+    (merge {:pandocir/type pandocir-type}
+           (cond (nil? c) {}
+                 (vector? c) (zipmap args c)
+                 :else {(first args) c}))
+    x))
 
-(defn ^:private inline-args->ir [ir-type c]
-  (case ir-type
-    :pandocir.type/str         {:pandocir/text c}
-
-    :pandocir.type/emph        {:pandocir/inlines c}
-
-    :pandocir.type/underline   {:pandocir/inlines c}
-
-    :pandocir.type/strong      {:pandocir/inlines c}
-
-    :pandocir.type/strikeout   {:pandocir/inlines c}
-
-    :pandocir.type/superscript {:pandocir/inlines c}
-
-    :pandocir.type/subscript   {:pandocir/inlines c}
-
-    :pandocir.type/small-caps  {:pandocir/inlines c}
-
-    :pandocir.type/quoted      {:pandocir.quote/type (first c)
-                                :pandocir/inlines (second c)}
-
-    :pandocir.type/cite        {:pandocir/citations (first c)
-                                :pandocir/inlines (second c)}
-
-    :pandocir.type/code        {:pandocir/attr (first c)
-                                :pandocir/text (second c)}
-
-    :pandocir.type/space       {}
-
-    :pandocir.type/soft-break  {}
-
-    :pandocir.type/line-break  {}
-
-    :pandocir.type/math        {:pandocir.math/type (first c)
-                                :pandocir/text (second c)}
-
-    :pandocir.type/raw-inline  {:pandocir/format (first c)
-                                :pandocir/text (second c)}
-
-    :pandocir.type/link        {:pandocir/attr (first c)
-                                :pandocir/inlines (second c)
-                                :pandocir/target (last c)}
-
-    :pandocir.type/image       {:pandocir/attr (first c)
-                                :pandocir/inlines (second c)
-                                :pandocir/target (last c)}
-
-    :pandocir.type/note        {:pandocir/blocks c}
-
-    :pandocir.type/span        {:pandocir/attr (first c)
-                                :pandocir/inlines (second c)}))
-
-(defn ^:private inline->ir-1 [{:keys [t c]}]
-  (let [ir-type (type->ir t)]
-    (merge {:pandocir/type ir-type}
-           (inline-args->ir ir-type c))))
-
-(defn inline->ir [inline]
-  (walk/postwalk #(if (:t %) (inline->ir-1 %) %) inline))
+(defn pandoc->ir [inline]
+  (walk/postwalk pandoc->ir-1 inline))
