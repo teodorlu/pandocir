b3b7b4c3351e95ef206c529dce475ea9c780a733
Teodor Heggelund <git@teod.eu>
2024-06-09 20:11:12 +0200

    Short import for builder

--- test/pandocir/builder_test.clj
+++ test/pandocir/builder_test.clj
@@ -1,35 +1,51 @@
 (ns pandocir.builder-test
   (:require
-   [pandocir.builder :as builder]
+   [pandocir.builder :as b]
    [clojure.test :refer [deftest is]]))
 
 (deftest str-test
   (is (= {:pandocir/type :pandocir.type/str, :pandocir/text "Hello,"}
-         (builder/str "Hello,"))))
+         (b/str "Hello,"))))
 
 (deftest space-test
   (is (= {:pandocir/type :pandocir.type/space}
-         (builder/space))))
+         (b/space))))
 
 (deftest para-test
   (is (= {:pandocir/type :pandocir.type/para,
           :pandocir/inlines
-          [(builder/str "Hello,")
-           (builder/space)]}
-         (builder/para (builder/str "Hello,") (builder/space)))))
+          [(b/str "Hello,")
+           (b/space)]}
+         (b/para (b/str "Hello,") (b/space)))))
 
 (deftest emph-test
   (is (= {:pandocir/type :pandocir.type/emph,
           :pandocir/inlines
-          [(builder/str "Pandoc")]}
-         (builder/emph (builder/str "Pandoc")))))
+          [(b/str "Pandoc")]}
+         (b/emph (b/str "Pandoc")))))
 
 (deftest doc-test
   (is (= {:blocks []}
-         (builder/doc {})))
+         (b/doc {})))
   (is (= {:blocks []
           :pandoc-api-version [1 23 1]}
-         (builder/doc {:pandoc-api-version [1 23 1]})))
+         (b/doc {:pandoc-api-version [1 23 1]})))
   (is (= {:blocks []
           :meta {}}
-         (builder/doc {:meta {}}))))
+         (b/doc {:meta {}})))
+  (is (= {:pandoc-api-version [1 23 1],
+          :meta {},
+          :blocks
+          [{:pandocir/type :pandocir.type/para,
+            :pandocir/inlines
+            [{:pandocir/type :pandocir.type/str, :pandocir/text "Hello,"}
+             {:pandocir/type :pandocir.type/space}
+             {:pandocir/type :pandocir.type/emph,
+              :pandocir/inlines
+              [{:pandocir/type :pandocir.type/str, :pandocir/text "Pandoc"}]}
+             {:pandocir/type :pandocir.type/str, :pandocir/text "!"}]}]}
+         (b/doc {:pandoc-api-version [1 23 1] :meta {}}
+                (b/para (b/str "Hello,")
+                        (b/space)
+                        (b/emph (b/str "Pandoc"))
+                        (b/str "!"))))))
