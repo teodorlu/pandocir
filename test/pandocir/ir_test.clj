ff7a1cdea9c951f8e9f480b10b3d66908585d8af
larstvei <larstvei@ifi.uio.no>
2024-06-18 13:18:52 +0200

    Add idempotent test
    I think we want the conversion to be idempotent.

--- test/pandocir/ir_test.clj
+++ test/pandocir/ir_test.clj
@@ -6,3 +6,8 @@
 (deftest bijective
   (doseq [[test-name test-data] pandoc-test-data]
     (is (= test-data (sut/ir->pandoc (sut/pandoc->ir test-data))))))
+
+(deftest idempotent
+  (doseq [[test-name test-data] pandoc-test-data]
+    (is (= (sut/pandoc->ir test-data)
+           (sut/pandoc->ir (sut/pandoc->ir test-data))))))
